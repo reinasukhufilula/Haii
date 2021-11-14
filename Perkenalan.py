7e5e3b3e2bae964b625d38d86adbd575b0afbb34
reinasukhufilula <94296171+reinasukhufilula@users.noreply.github.com>
2021-11-14 20:57:56 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,7 @@
+Nama = "Reina Sukhufil Ula"
+nim = "212410102038"
+Prodi = "Teknologi Informasi"
+Fakultas = "Ilmu Komputer"
+Asal = "Kediri"
+
+print("Haloo!!! Perkenalkan nama saya ", Nama, " NIM ", nim, " dari Prodi ", Prodi, " Fakultas ", Fakultas, " Universitas jember. ", " asal dari ", Asal, " Salam Kenal semuaaaa")
